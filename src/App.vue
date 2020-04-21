2e5c05e423b6a5c8ebda6e2aa30c65a7b3da4c94
surly1996 <449349576@qq.com>
2020-04-21 14:25:21 +0800

    初始化计数器项目的基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <my-add></my-add>
+
+    <p>------------</p>
+
+    <my-sub></my-sub>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Add from './components/Add.vue'
+import Sub from './components/Sub.vue'
 
 export default {
-  name: 'App',
+  data() {
+    return {}
+  },
   components: {
-    HelloWorld
+    'my-add': Add,
+    'my-sub': Sub
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Sub.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>当前最新的 count 值为:</h3>
+    <button>-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
